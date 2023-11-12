04364e060f47c49ecec13369a7ed00df51666b95
Bastres <109953748+Bastres@users.noreply.github.com>
2023-11-12 01:13:15 -0300

    Cliente_RPC readaptado

--- Cliente_RPC.py
+++ Cliente_RPC.py
@@ -3,7 +3,7 @@ import socket
 import inspect
 from threading import Thread
 
-class RPC_Cliente:
+class Cliente_RPC:
     def __init__(self, host:str='localhost', porta:int=8080) -> None:
         self.sock = None
         self.endereco = (host, porta)
@@ -21,12 +21,57 @@ class RPC_Cliente:
         except:
             pass
             
-    def __getattr__(self nome: str):
-        def excecutar(*args, **kwargs):
-            self.sock.sendall(json.dumps((nome, args, kwargs)).encode())
+    def __getattr__(self, __nome: str):
+        def executar(*args, **kwargs):
+            self.sock.sendall(json.dumps((__nome, args, kwargs)).encode())
 
-            resposta = json.loads(self.sock.recv(SIZE).decode())
+            resposta = json.loads(self.sock.recv(1024).decode())
    
             return resposta
         
-        return excecutar
+        return executar
+        
+
+server = Cliente_RPC()
+
+server.conectar()
+
+while True:
+    try:
+        x = int(input('O que deseja fazer?\n\n1. ADD\n2. SUB\n3. MULT\n4. DIV\n\n'))
+        
+        if x == 1:
+            
+            a = int(input('\nEscolha o primeiro valor para a adicao:\n>'))
+            b = int(input('\nEscolha o segundo valor para a adicao:\n>'))
+            
+            print(f'\nResultado: {server.add(a, b)}\n')
+            
+        elif x == 2:
+        
+            a = int(input('\nEscolha o primeiro valor para a subtracao:\n>'))
+            b = int(input('\nEscolha o segundo valor para a subtracao:\n>'))
+                
+            print(f'\nResultado: {server.sub(a, b)}\n')
+            
+        elif x == 3:
+        
+            a = int(input('\nEscolha o primeiro valor para a multiplicacao:\n>'))
+            b = int(input('\nEscolha o segundo valor para a multiplicacao:\n>'))
+            
+            print(f'\nResultado: {server.mult(a,b)}\n')
+            
+        elif x == 4:
+        
+            a = int(input('\nEscolha o primeiro valor para a divisao:\n>'))
+            b = int(input('\nEscolha o segundo valor para a divisao:\n>'))
+            
+            print(f'\nResultado: {server.div(a,b)}\n')    
+            
+        else:
+            print('\nTente novamente.\n')
+        
+    except KeyboardInterrupt:
+        break
+
+server.desconectar()
